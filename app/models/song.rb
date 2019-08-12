996a69dea45e625a82ca8f218db3b0c34097c0cb
Alan Krajina <alankrajina88@gmail.com>
2019-08-12 18:31:12 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,20 +5,9 @@ class Song < ApplicationRecord
     song.validates :release_year, presence: true
     song.validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }
   end
-#  binding.pry
-
-#  validates :release_year,  presence: true, if: :release_year?
-
-
   validates :artist_name, presence: true
-
-
 
   def released?
     released
   end
 end
-
-
-# Must be less than or equal to the current year
-#   8) Song is invalid when the release year is in the future
