a8bfa5a9853cf6a0afa2ddef51eef8129b5b7f29
Alan Krajina <alankrajina88@gmail.com>
2019-08-12 18:03:21 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :released, inclusion: { in: [ true, false ] }
   validates :release_year, presence: true, if: :released_true?
-  validates_date :release_year, on_or_before: lambda { Date.current }
+  validates :release_year, on_or_before: lambda { Date.current }
 
 
   validates :artist_name, presence: true
