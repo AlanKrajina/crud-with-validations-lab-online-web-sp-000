9d9d3b0a8f04f10e48ee992f5304d56df0815627
Alan Krajina <alankrajina88@gmail.com>
2019-08-12 18:20:13 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,8 @@ class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :released, inclusion: { in: [ true, false ] }
   validates :release_year, presence: true, if: :released_true?
+  validates :release_year, presence: false, if: :released_true?
+
 #  binding.pry
 
   validates :release_year,  presence: true, if: :release_year?
