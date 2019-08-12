45209789abf071f71626572d98836419b20ef940
Alan Krajina <alankrajina88@gmail.com>
2019-08-12 18:05:02 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,8 @@
 class Song < ApplicationRecord
-  attr_accessor :month, :day, :year
-
-
   validates :title, presence: true, uniqueness: true
   validates :released, inclusion: { in: [ true, false ] }
   validates :release_year, presence: true, if: :released_true?
-  validate :validate_created_at
+  validates :release_year, on_or_before: lambda { Date.current }
 
 
   validates :artist_name, presence: true
@@ -13,18 +10,6 @@ class Song < ApplicationRecord
 
   def released_true?
     released == true
-  end
-
-  def convert_created_at
-    begin
-      self.created_at = Date.civil(self.year.to_i, self.month.to_i, self.day.to_i)
-    rescue ArgumentError
-      false
-    end
-  end
-
-  def validate_created_at
-    errors.add("Created at date", "is invalid.") unless convert_created_at
   end
 
 end
