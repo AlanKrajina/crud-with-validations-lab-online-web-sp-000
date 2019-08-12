f32c1e437a16fd272b737af0d293a605ca722137
Alan Krajina <alankrajina88@gmail.com>
2019-08-12 18:46:53 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,7 +43,7 @@ class SongsController < ApplicationController
   
   private
   
-  def song_params(*args)
+  def song_params
 	  params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
 	end
   
