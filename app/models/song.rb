32fcf8231770c0cfbce446dec13a2812fff78312
Alan Krajina <alankrajina88@gmail.com>
2019-08-12 18:00:24 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,7 @@ class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :released, inclusion: { in: [ true, false ] }
   validates :release_year, presence: true, if: :released_true?
+  validates :release_year, presence: false, if: :released_true?
 
 
   validates :artist_name, presence: true
