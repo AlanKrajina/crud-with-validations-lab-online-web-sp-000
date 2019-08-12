7c659e1228d7241bb960ddd248f6463e6589f8c9
Alan Krajina <alankrajina88@gmail.com>
2019-08-12 18:47:00 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(song_params(:title, :released, :release_year, :artist_name, :genre))
+    @song = Song.new(song_params)
     binding.pry
     if @song.save
       redirect_to song_path(@song)
